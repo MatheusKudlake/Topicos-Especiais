a64768f79e41a37f658f5b48e586ce4e65810fb4
MatheusKudlake <mkudlake@gmail.com>
2025-03-25 21:38:04 -0300

    Aula 04

---
+++ Aula04/script.js
@@ -0,0 +1,12 @@
+let texto = document.getElementById("texto");
+//Laço de repetilção
+
+//for(let i=0; i<10; i++){
+//    texto.innerHTML += i + "<br>";
+//}
+
+//Operadores ternários
+let fruta = "Abacaxi";
+let sabor = fruta == "Abacaxi" ? "Azedo" : "Salgado";
+
+texto.innerHTML = fruta + "<br>" + sabor;
